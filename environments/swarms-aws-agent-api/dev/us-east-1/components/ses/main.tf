d4b64fb1e31400f29dc2eb8e70e6af97337df01a
mike dupont <mike.dupont@introspector.local>
2025-01-09 14:16:21 -0500

    applied

--- environments/swarms-aws-agent-api/dev/us-east-1/components/ses/main.tf
+++ environments/swarms-aws-agent-api/dev/us-east-1/components/ses/main.tf
@@ -9,15 +9,15 @@ variable "domain" {}
 #  tags = module.this.tags
 #}
 
-module "ses" {
-  source = "git::https://github.com/cloudposse/terraform-aws-ses.git"
-  domain        = var.domain
-  #  zone_id       = var.zone_id
-    zone_id       = aws_route53_zone.private_dns_zone.zone_id
-  verify_dkim   = var.verify_dkim
-  verify_domain = var.verify_domain
-  ses_group_name = var.group_name
-  context = module.this.context
-  #ses_group_enabled = false
-  ses_user_enabled = false
-}
+# module "ses" {
+#   source = "git::https://github.com/cloudposse/terraform-aws-ses.git"
+#   domain        = var.domain
+#   #  zone_id       = var.zone_id
+# #    zone_id       = aws_route53_zone.private_dns_zone.zone_id
+#   verify_dkim   = var.verify_dkim
+#   verify_domain = var.verify_domain
+#   ses_group_name = var.group_name
+#   context = module.this.context
+#   #ses_group_enabled = false
+#   ses_user_enabled = false
+# }
