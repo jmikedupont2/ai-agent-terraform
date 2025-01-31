36f5af8969edc06be7f9762bb3408510cc358d71
mike dupont <mike.dupont@introspector.local>
2025-01-31 16:42:11 -0500

    working now

--- cloudformation/main.tf
+++ cloudformation/main.tf
@@ -1,8 +1,13 @@
+
 provider "aws" {
   region  = "us-west-1"
-  profile = "mdupont"
+#  profile = "mdupont"
 }
 
+variable groq_key {}
+variable twitter_password {}
+variable twitter_user_name {}
+variable twitter_mail {}
 locals {   ami_name = "ubuntu-minimal/images/hvm-ssd-gp3/ubuntu-noble-24.04-arm64-minimal-*" }
 data "aws_ami" "ami" { # slow
     most_recent      = true
@@ -17,7 +22,11 @@ resource "aws_cloudformation_stack" "eliza_stack" {
 #  template_body = file("cloudformation.yml")
   template_body = file("ec2.yml")
   parameters = {
-      AmiId = data.aws_ami.ami.id
+    AmiId = data.aws_ami.ami.id
+    GroqKey = var.groq_key
+    TwitterPassword= var.twitter_password
+    TwitterUserName = var.twitter_user_name 
+    TwitterEmail = var.twitter_mail
  
    }  
 }
