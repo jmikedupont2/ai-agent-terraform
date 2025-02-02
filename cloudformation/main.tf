f95861528466f6ea51ec1f639b416c207610bc3c
mike dupont <mike.dupont@introspector.local>
2025-02-02 00:43:38 -0500

    regions

--- cloudformation/main.tf
+++ cloudformation/main.tf
@@ -1,7 +1,34 @@
 
+
 provider "aws" {
   region  = "us-west-1"
 #  profile = "mdupont"
+}
+
+provider "aws" {
+  alias  = "uswest1"
+  region  = "us-west-1"
+#  profile = "mdupont"
+}
+
+provider "aws" {
+  alias  = "useast1"
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias  = "useast2"
+  region = "us-east-2"
+}
+
+provider "aws" {
+  alias  = "eucentral1"
+  region = "eu-central-1"
+}
+
+provider "aws" {
+  alias  = "apsoutheast1"
+  region = "ap-southeast-1"
 }
 
 variable groq_key {}
@@ -11,28 +38,22 @@ variable twitter_mail {}
 variable lt_version {
   default = "1"
 }
-locals {   ami_name = "ubuntu-minimal/images/hvm-ssd-gp3/ubuntu-noble-24.04-arm64-minimal-*" }
-data "aws_ami" "ami" { # slow
-    most_recent      = true
-    owners          = [ 679593333241 ]
-    name_regex       = "^${local.ami_name}"
-  }
 
-resource "aws_cloudformation_stack" "eliza_stack" {
-  name = "tine-agent"
-  capabilities = [ "CAPABILITY_NAMED_IAM" ]
+# resource "aws_cloudformation_stack" "eliza_stack" {
+#   name = "tine-agent"
+#   capabilities = [ "CAPABILITY_NAMED_IAM" ]
 
-#  template_body = file("cloudformation.yml")
-  template_body = file("ec2.yml")
-  parameters = {
-    AmiId = data.aws_ami.ami.id
-    GroqKey = var.groq_key
-    TwitterPassword= var.twitter_password
-    TwitterUserName = var.twitter_user_name 
-    TwitterEmail = var.twitter_mail
-    LaunchTemplateVersion = var.lt_version
-   }  
-}
+# #  template_body = file("cloudformation.yml")
+#   template_body = file("ec2.yml")
+#   parameters = {
+#     AmiId = data.aws_ami.ami.id
+#     GroqKey = var.groq_key
+#     TwitterPassword= var.twitter_password
+#     TwitterUserName = var.twitter_user_name 
+#     TwitterEmail = var.twitter_mail
+#     LaunchTemplateVersion = var.lt_version
+#    }  
+# }
 
 resource "aws_s3_bucket" "template_bucket" {
   bucket = "zos-solfunmeme-tine-cf-template"  # Replace with your desired bucket name
@@ -71,6 +92,45 @@ resource "aws_s3_object" "cloudformation_template" {
 
 output "template_url" {
   value = "https://${aws_s3_bucket.template_bucket.bucket_regional_domain_name}/${aws_s3_object.cloudformation_template.key}"
+}
+module region_useast1 {
+    source = "./regional_outpost"
+    providers ={        aws = aws.useast1    }
+    region = "us-east-1"
+}
+
+module region_useast2 {
+    source = "./regional_outpost"
+    providers ={        aws = aws.useast2    }
+    region = "us-east-2"
+}
+
+module region_uswest1 {
+    source = "./regional_outpost"
+    providers ={        aws = aws.uswest1    }
+    region = "us-west-1"
+}
+
+module region_eucentral1 {
+    source = "./regional_outpost"
+    providers ={        aws = aws.eucentral1    }
+    region = "eu-central-1"
+}
+
+module region_apsoutheast1 {
+    source = "./regional_outpost"
+    providers ={        aws = aws.apsoutheast1    }
+    region = "ap-southeast-1"
+}
+
+output "regions" {
+  value = [
+    module.region_useast2.full_template_url,
+    module.region_useast1.full_template_url,
+    module.region_uswest1.full_template_url,
+    module.region_eucentral1.full_template_url,
+    module.region_apsoutheast1.full_template_url
+  ]
 }
 
 # resource "aws_cloudformation_type" "tine" {
