07d54390a4eb2301119c6a5216b20c9c5cb47844
mike dupont <mike.dupont@introspector.local>
2025-02-02 21:40:46 -0500

    fix

--- cloudformation/main.tf
+++ cloudformation/main.tf
@@ -47,44 +47,6 @@ variable "lt_version" {
   default = "1"
 }
 
-resource "aws_s3_bucket" "template_bucket" {
-  bucket        = "zos-solfunmeme-tine-cf-template" # Replace with your desired bucket name
-  force_destroy = true
-}
-
-resource "aws_s3_bucket_public_access_block" "template_bucket_public_access" {
-  bucket                  = aws_s3_bucket.template_bucket.id
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-resource "aws_s3_bucket_policy" "allow_public_read" {
-  bucket = aws_s3_bucket.template_bucket.id
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.template_bucket.arn}/*"
-      }
-    ]
-  })
-}
-
-resource "aws_s3_object" "cloudformation_template" {
-  bucket = aws_s3_bucket.template_bucket.id
-  key    = "zos-solfunmeme-tine-the-introspector-is-not-eliza-stack-template-one-click-installer-dev.yaml" # Replace with your desired file name
-  source = "ec2.yml"                                                                                       # Replace with the path to your template file
-}
-
-output "template_url" {
-  value = "https://${aws_s3_bucket.template_bucket.bucket_regional_domain_name}/${aws_s3_object.cloudformation_template.key}"
-}
 module "region_useast1" {
   source    = "./regional_outpost"
   providers = { aws = aws.useast1 }
