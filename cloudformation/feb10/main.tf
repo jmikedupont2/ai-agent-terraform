8e05553d8f49477bd344656d540e0631bd446686
mike dupont <mike.dupont@introspector.local>
2025-02-12 13:10:48 -0500

    new isnstaller
    totally untested

--- cloudformation/feb10/main.tf
+++ cloudformation/feb10/main.tf
@@ -169,70 +169,70 @@ provider "aws" {
 # now deploy
 
 module "region_apnortheast1" {
-  source    = "../regional_outpost"
+  source    = "../regional_outpost_reuse"
   providers = { aws = aws.apnortheast1 }
   region    = "ap-northeast-1"
   patch     = local.patch
 }
 
 module "region_apnortheast2" {
-  source    = "../regional_outpost"
+  source    = "../regional_outpost_reuse"
   providers = { aws = aws.apnortheast2 }
   region    = "ap-northeast-2"
   patch     = local.patch
 }
 
 module "region_apnortheast3" {
-  source    = "../regional_outpost"
+  source    = "../regional_outpost_reuse"
   providers = { aws = aws.apnortheast3 }
   region    = "ap-northeast-3"
   patch     = local.patch
 }
 
 module "region_apsouth1" {
-  source    = "../regional_outpost"
+  source    = "../regional_outpost_reuse"
   providers = { aws = aws.apsouth1 }
   region    = "ap-south-1"
   patch     = local.patch
 }
 
 module "region_apsoutheast1" {
-  source    = "../regional_outpost"
+  source    = "../regional_outpost_reuse"
   providers = { aws = aws.apsoutheast1 }
   region    = "ap-southeast-1"
   patch     = local.patch
 }
 
 module "region_apsoutheast2" {
-  source    = "../regional_outpost"
+  source    = "../regional_outpost_reuse"
   providers = { aws = aws.apsoutheast2 }
   region    = "ap-southeast-2"
   patch     = local.patch
 }
 
 module "region_cacentral1" {
-  source    = "../regional_outpost"
+  source    = "../regional_outpost_reuse"
   providers = { aws = aws.cacentral1 }
   region    = "ca-central-1"
   patch     = local.patch
 }
 
 module "region_eucentral1" {
-  source    = "../regional_outpost"
+  source    = "../regional_outpost_reuse"
   providers = { aws = aws.eucentral1 }
   region    = "eu-central-1"
   patch     = local.patch
 }
 
 module "region_eunorth1" {
-  source    = "../regional_outpost"
+  source    = "../regional_outpost_reuse"
   providers = { aws = aws.eunorth1 }
   region    = "eu-north-1"
   patch     = local.patch
 }
 
 module "region_euwest1" {
-  source    = "../regional_outpost"
+  source    = "../regional_outpost_reuse"
   providers = { aws = aws.euwest1 }
   region    = "eu-west-1"
   patch     = local.patch
@@ -240,67 +240,67 @@ module "region_euwest1" {
 
 
 module "region_euwest2" {
-  source    = "../regional_outpost"
+  source    = "../regional_outpost_reuse"
   providers = { aws = aws.euwest2 }
   region    = "eu-west-2"
   patch     = local.patch
 }
 
 module "region_euwest3" {
-  source    = "../regional_outpost"
+  source    = "../regional_outpost_reuse"
   providers = { aws = aws.euwest3 }
   region    = "eu-west-3"
   patch     = local.patch
 }
 
 module "region_saeast1" {
-  source    = "../regional_outpost"
+  source    = "../regional_outpost_reuse"
   providers = { aws = aws.saeast1 }
   region    = "sa-east-1"
   patch     = local.patch
 }
 
 module "region_useast1" {
-  source    = "../regional_outpost"
+  source    = "../regional_outpost_reuse"
   providers = { aws = aws.useast1 }
   region    = "us-east-1"
   patch     = local.patch
 }
 
 module "region_useast2" {
-  source    = "../regional_outpost"
+  source    = "../regional_outpost_reuse"
   providers = { aws = aws.useast2 }
   region    = "us-east-2"
   patch     = local.patch
 }
 
 module "region_uswest1" {
-  source    = "../regional_outpost"
+  source    = "../regional_outpost_reuse"
   providers = { aws = aws.uswest1 }
   region    = "us-west-1"
   patch     = local.patch
 }
 
 module "region_uswest2" {
-  source    = "../regional_outpost"
+  source    = "../regional_outpost_reuse"
   providers = { aws = aws.uswest2 }
   region    = "us-west-2"
   patch     = local.patch
 }
 
-#module "region_apsouth2" { source    = "../regional_outpost" providers = { aws = aws.apsouth2 } region    = "ap-south-2"}
-#module "region_apsoutheast3" { source    = "../regional_outpost" providers = { aws = aws.apsoutheast3 } region    = "ap-southeast-3"}
-#module "region_apsoutheast4" { source    = "../regional_outpost" providers = { aws = aws.apsoutheast4 } region    = "ap-southeast-4"}
-#module "region_apsoutheast5" {  source    = "../regional_outpost"  providers = { aws = aws.apsoutheast5 }  region    = "ap-southeast-5"}
-#module "region_apsoutheast7" {  source    = "../regional_outpost"  providers = { aws = aws.apsoutheast7 }  region    = "ap-southeast-7"}
-#module "region_cawest1" {  source    = "../regional_outpost"  providers = { aws = aws.cawest1 }  region    = "ca-west-1"}
-#module "region_eucentral2" {  source    = "../regional_outpost"  providers = { aws = aws.eucentral2 }  region    = "eu-central-2"}
-#module "region_eusouth1" {  source    = "../regional_outpost"  providers = { aws = aws.eusouth1 }  region    = "eu-south-1"}
-#module "region_eusouth2" {  source    = "../regional_outpost"  providers = { aws = aws.eusouth2 }  region    = "eu-south-2"}
-#module "region_ilcentral1" {  source    = "../regional_outpost"  providers = { aws = aws.ilcentral1 }  region    = "il-central-1"}
-#module "region_mecentral1" {  source    = "../regional_outpost"  providers = { aws = aws.mecentral1 }  region    = "me-central-1"}
-#module "region_mesouth1" {  source    = "../regional_outpost"  providers = { aws = aws.mesouth1 }  region    = "me-south-1"}
-#module "region_mxcentral1" {  source    = "../regional_outpost"  providers = { aws = aws.mxcentral1 }  region    = "mx-central-1"}
+#module "region_apsouth2" { source    = "../regional_outpost_reuse" providers = { aws = aws.apsouth2 } region    = "ap-south-2"}
+#module "region_apsoutheast3" { source    = "../regional_outpost_reuse" providers = { aws = aws.apsoutheast3 } region    = "ap-southeast-3"}
+#module "region_apsoutheast4" { source    = "../regional_outpost_reuse" providers = { aws = aws.apsoutheast4 } region    = "ap-southeast-4"}
+#module "region_apsoutheast5" {  source    = "../regional_outpost_reuse"  providers = { aws = aws.apsoutheast5 }  region    = "ap-southeast-5"}
+#module "region_apsoutheast7" {  source    = "../regional_outpost_reuse"  providers = { aws = aws.apsoutheast7 }  region    = "ap-southeast-7"}
+#module "region_cawest1" {  source    = "../regional_outpost_reuse"  providers = { aws = aws.cawest1 }  region    = "ca-west-1"}
+#module "region_eucentral2" {  source    = "../regional_outpost_reuse"  providers = { aws = aws.eucentral2 }  region    = "eu-central-2"}
+#module "region_eusouth1" {  source    = "../regional_outpost_reuse"  providers = { aws = aws.eusouth1 }  region    = "eu-south-1"}
+#module "region_eusouth2" {  source    = "../regional_outpost_reuse"  providers = { aws = aws.eusouth2 }  region    = "eu-south-2"}
+#module "region_ilcentral1" {  source    = "../regional_outpost_reuse"  providers = { aws = aws.ilcentral1 }  region    = "il-central-1"}
+#module "region_mecentral1" {  source    = "../regional_outpost_reuse"  providers = { aws = aws.mecentral1 }  region    = "me-central-1"}
+#module "region_mesouth1" {  source    = "../regional_outpost_reuse"  providers = { aws = aws.mesouth1 }  region    = "me-south-1"}
+#module "region_mxcentral1" {  source    = "../regional_outpost_reuse"  providers = { aws = aws.mxcentral1 }  region    = "mx-central-1"}
 
 locals {
   all_regions = [
---
+++ cloudformation/regional_outpost_reuse/main.tf
@@ -0,0 +1,83 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+variable "region" {}
+variable "patch" {} # was v3
+
+# resource "aws_s3_bucket" "template_bucket" {
+#   count = 0 # dont create now
+#   bucket        = "zos-solfunmeme-tine-cf-template-${var.region}" # Replace with your desired bucket name
+#   force_destroy = true
+# }
+
+data "aws_s3_bucket" "template_bucket" {
+  bucket        = "zos-solfunmeme-tine-cf-template-${var.region}" # Replace with your desired bucket name
+
+}
+
+# resource "aws_s3_bucket_public_access_block" "template_bucket_public_access" {
+#  count = 0 # dont create now
+#   bucket                  = aws_s3_bucket.template_bucket.id
+#   block_public_acls       = false
+#   block_public_policy     = false
+#   ignore_public_acls      = false
+#   restrict_public_buckets = false
+# }
+
+# resource "aws_s3_bucket_policy" "allow_public_read" {
+#   count = 0 # dont create now
+#   bucket = aws_s3_bucket.template_bucket.id
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Sid       = "PublicReadGetObject"
+#         Effect    = "Allow"
+#         Principal = "*"
+#         Action    = "s3:GetObject"
+#         Resource  = "${aws_s3_bucket.template_bucket.arn}/*"
+#       }
+#     ]
+#   })
+# }
+
+resource "aws_s3_object" "cloudformation_template" {
+  bucket = data.aws_s3_bucket.template_bucket.id
+  key    = "zos-solfunmeme-tine-the-introspector-is-not-eliza-stack-template-one-click-installer-dev-${var.patch}.yaml" # Replace with your desired file name
+  source = "ec2.yml"                                                                                                    # Replace with the path to your template file
+}
+
+
+data "aws_ami" "ami" { # slow
+  most_recent = true
+  owners      = [679593333241] # ubuntu
+  name_regex  = "^${local.ami_name}"
+}
+
+locals {
+  ami_name     = "ubuntu-minimal/images/hvm-ssd-gp3/ubuntu-noble-24.04-arm64-minimal-*"
+  template_url = "https://${data.aws_s3_bucket.template_bucket.bucket_regional_domain_name}/${aws_s3_object.cloudformation_template.key}"
+}
+
+locals {
+  cf_template_url = "https://${var.region}.console.aws.amazon.com/cloudformation/home?region=${var.region}#/stacks/quickcreate?templateURL=${local.template_url}&stackName=zos-solfunmeme-tine-the-introspector-is-not-eliza-stack-template-one-click-installer&param_S3BucketPattern=tine_agent_*&param_GroqKey=&param_TokenizerImage=h4ckermike%2Farm64-tokenizers%3Afeature-arm64&param_TwitterPassword=&param_NameTag=tine-dev&param_AgentCodeName=tine_agent_4&param_SSMParameterPattern=tine_agent_*&param_TwitterUserName=&param_LaunchTemplateVersion=1&param_TwitterEmail=&param_AgentImage=h4ckermike%2Felizaos-eliza%3Afeature-arm64_fastembed&param_AmiId=${data.aws_ami.ami.id}"
+  image_url       = "![Launch ${var.region} Stack](https://cdn.rawgit.com/buildkite/cloudformation-launch-stack-button-svg/master/launch-stack.svg)"
+}
+
+output "full_template_url" {
+  value = local.template_url
+}
+
+output "full_html_url" {
+  value = "* ${var.region} [${local.image_url}](${local.cf_template_url})"
+}
+
+output "ami_id" {
+  value = data.aws_ami.ami.id
+}
